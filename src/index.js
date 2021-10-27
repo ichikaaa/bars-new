462012883478f883efe5684c13a07cd8446f0d1c
ichikaaa <aikarosemanguba@gmail.com>
2021-10-27 16:03:47 +0800

    new init

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const { uploadFile, clear } = require('./middleware/uploadFile');
-const { content } = require('./fileReader');
-const fs = require('fs');
-require('./database/bars_db');
-const { Billing, Store } = require('./model/billings');
-const moment = require('moment');
+const express = require("express");
+const { uploadFile, clear } = require("./middleware/uploadFile");
+const { content } = require("./fileReader");
+const fs = require("fs");
+require("./database/bars_db");
+const { Billing, Store } = require("./model/billings");
+const moment = require("moment");
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = 3000 || "https://bars-330206.as.r.appspot.com";
 
 app.post(
-  '/upload',
+  "/upload",
   clear,
-  uploadFile.single('upload'),
+  uploadFile.single("upload"),
   (req, res) => {
-    const stats = fs.statSync('csv-txt-files/upload.txt');
+    const stats = fs.statSync("csv-txt-files/upload.txt");
     if (stats.size === 0) {
-      throw new Error('No request(s) to read from the input file.');
+      throw new Error("No request(s) to read from the input file.");
     }
     const client = content();
     console.log(`Processing Request with three parameters`);
     Store.deleteMany({}, (err, user) => {
       if (err) {
-        console.log('Cannot pull out');
+        console.log("Cannot pull out");
       }
     });
     let n = 0;
@@ -33,17 +33,18 @@ app.post(
       Billing.find(client[i], (error, user) => {
         try {
           const wait = new Store({
-            billingCycle: '0' + String(user[0].billingCycle),
-            startDate: moment(user[0].startDate).format('MM/DD/YYYY'),
-            endDate: moment(user[0].endDate).format('MM/DD/YYYY'),
+            billingCycle: "0" + String(user[0].billingCycle),
+            startDate: moment(user[0].startDate).format("MM/DD/YYYY"),
+            endDate: moment(user[0].endDate).format("MM/DD/YYYY"),
             firstName: user[0].account.customer.firstName,
             lastName: user[0].account.customer.lastName,
-            amount: user[0].amount});
+            amount: user[0].amount,
+          });
           wait.save();
           n++;
         } catch (error) {
-          res.status(400).send({ error: 'No Record Found!' });
-          console.log('error: No Record Found!');
+          res.status(400).send({ error: "No Record Found!" });
+          console.log("error: No Record Found!");
         }
       });
     }
@@ -52,8 +53,8 @@ app.post(
       if (!(n === 0)) {
         Store.find({}, (err, user) => {
           res.status(200).send(user);
-        }).select('-_id');
-        console.log('Successfully displayed billing request!');
+        }).select("-_id");
+        console.log("Successfully displayed billing request!");
       }
     }, 400);
   },
